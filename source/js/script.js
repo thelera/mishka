518869c4ab9cfb538cfb034f03828d7a774368ec
Lera Bilshchuk <leratheone@gmail.com>
2020-01-08 10:56:08 +0300

    add catalog.html markup and styles

--- source/js/script.js
+++ source/js/script.js
@@ -3,9 +3,12 @@
 var menu = document.querySelector(".menu");
 var menuToggle = document.querySelector(".menu__toggle");
 
-menu.classList.remove("menu--no-js");
 
-//if (menuToggle) {
+
+menu.classList.remove("menu--no-js");
+menu.classList.remove("menu--opened");
+
+if (menuToggle) {
   menuToggle.addEventListener("click", function () {
     if (menu.classList.contains("menu--opened")) {
       menu.classList.remove("menu--opened");
@@ -13,4 +16,38 @@ menu.classList.remove("menu--no-js");
       menu.classList.add("menu--opened");
     }
   });
-//}
+}
+
+// модальное окно
+var overlay = document.querySelector(".overlay");//поле вне формы
+var addToCartModal = document.querySelector(".add-to-cart-modal");
+var buyButton = document.querySelector(".week-product__buy-button");
+
+if (buyButton) {
+buyButton.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    addToCartModal.classList.add("add-to-cart-modal--show");
+    overlay.classList.add("overlay--show");
+  });
+}
+
+//закрываем форму при клике вне формы
+if(overlay) {
+  overlay.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    addToCartModal.classList.remove("add-to-cart-modal--show");
+    overlay.classList.remove("overlay--show");
+  });
+}
+
+//выход из формы при нажатии клавиши escape
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (addToCartModal.classList.contains("add-to-cart-modal--show")) {
+      evt.preventDefault();
+      addToCartModal.classList.remove("add-to-cart-modal--show");
+      overlay.classList.remove("overlay--show");
+    }
+  }
+});
+
